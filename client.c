e5c78918f487ec5c6d7a508fac69763a59b7b803
Shiroii || Lio <139677855+Shiiroii@users.noreply.github.com>
2025-02-03 15:02:05 +0100

    Update client.c

--- client.c
+++ client.c
@@ -15,17 +15,17 @@
 void	is_it_done(int sig)
 {
 	(void)sig;
-	g_finished = true;
+	g_finished = true; // variable globale me permettant de savoir quand le signal a ete envoye et recu //
 }
 
 void	ft_transfer_message(pid_t srv_pid, char *message)
 {
 	unsigned char		car;
 	int					bits;
-	struct sigaction	check;
+	struct sigaction	check; // sigaction permet de gerer les processus d'un signal specifique
 
-	check.sa_handler = is_it_done;
-	check.sa_flags = SA_RESTART;
+	check.sa_handler = is_it_done; // recoit a chaque envoi du signal le numero de celui-ci, permettant alors de faire une action a chaque fois // 
+	check.sa_flags = SA_RESTART; // permet de remettre a zero lors de linitialisation des signaux //
 	sigemptyset(&check.sa_mask);
 	sigaction(SIGUSR1, &check, NULL);
 	while (*message)
@@ -36,11 +36,11 @@ void	ft_transfer_message(pid_t srv_pid, char *message)
 		{
 			g_finished = false;
 			if (car & (1 << 7))
-				kill(srv_pid, SIGUSR1);
+				kill(srv_pid, SIGUSR1); // kill pernet d'envoyer un signal //
 			else
 				kill(srv_pid, SIGUSR2);
 			while (!g_finished)
-				usleep(400);
+				usleep(400); // met en pause l'envoie du signal pendant un tres petit temps //
 			car <<= 1;
 		}
 		message++;
